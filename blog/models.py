4721a93c98e904f9b9b2331b03eb482c38bbcc1a
AlgoClassWork <kyxec.play@gmail.com>
2025-09-28 10:51:29 +0600

    Add files via upload

--- blog/models.py
+++ blog/models.py
@@ -3,8 +3,11 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='post_images/', blank=True, null=True)
+    title = models.CharField(verbose_name='Заголовок', max_length=200)
+    content = models.TextField(verbose_name='Содержание')
+    created_at = models.DateTimeField(verbose_name='Дата', auto_now_add=True)
+    author = models.ForeignKey(User, verbose_name='Автор', on_delete=models.CASCADE)
+    image = models.ImageField(verbose_name='Изображение', upload_to='post_images/', blank=True, null=True)  
+
+    def __str__(self):
+        return self.title
