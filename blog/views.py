fc5dfb85c847161b3d91f3cab1d610ad7e59c8c8
AlgoClassWork <kyxec.play@gmail.com>
2025-10-05 10:51:09 +0600

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .models import Post
 from .forms import PostForm
@@ -13,8 +13,15 @@ def post_detail(request, id):
     post = get_object_or_404(Post, id=id)
     return render(request, 'post_detail.html', {'post':post})
 
-
 # http://127.0.0.1:8000/create/
 def post_create(request):
     form = PostForm()
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('home_page')
+
     return render(request, 'post_create.html', {'form':form})
